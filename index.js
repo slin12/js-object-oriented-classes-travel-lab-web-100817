d28e33ba867f9574fe4d176207e9380506b07804
slin12 <s.lin12@gmail.com>
2017-11-21 12:24:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+class Driver {
+  constructor(name, startDay) {
+    this.name = name;
+    this.startDate = new Date(startDay);
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear() - 1;
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endLocation = endLocation;
+  }
+
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    const northSouthBlocks = Math.abs(this.beginningLocation.vertical - this.endLocation.vertical)
+    const eastWestBlocks = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endLocation.horizontal));
+    return northSouthBlocks + eastWestBlocks;
+  }
+
+  estimatedTime(peak) {
+    if (peak === true) {
+      return this.blocksTravelled() / 2;
+    }
+    else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+}
